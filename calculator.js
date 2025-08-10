11390928404b059bfe2ba88a1561950f5cd7f886
Suman Bera <135948367+ONEABOVEALL01@users.noreply.github.com>
2025-08-10 14:22:53 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,50 @@
+
+
+let string = "";
+let button = document.querySelectorAll("button");
+Array.from(button).forEach((button) => {
+    button.addEventListener('click',(e)=>{
+
+        if(e.target.innerHTML == '='){
+           
+            try {
+                string = eval(string);
+                document.querySelector('input').value = string;
+            } catch (error) {
+                document.querySelector('input').value = "Error...";
+                string = "";
+            }
+        }
+        else if(e.target.innerHTML == 'Clr'){
+            string = "";
+         document.querySelector('input').value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.slice(0,-1);
+                document.querySelector('input').value = string;
+            
+        }
+        else{
+        console.log(e.target)
+
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value = string;
+        }
+
+
+    });
+}
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
